7a1f2ed820b22d3af0238c8661d327c4ff413a74
Tyler Suehr <tylersuehr7@yahoo.com>
2017-07-03 13:14:52 -0400

    Created mappers for the people.

--- app/src/main/java/com/tylersuehr/cleanarchitecture/data/mappers/IEntityMapper.java
+++ app/src/main/java/com/tylersuehr/cleanarchitecture/data/mappers/IEntityMapper.java
@@ -1,14 +1,26 @@
 package com.tylersuehr.cleanarchitecture.data.mappers;
-
 import android.content.ContentValues;
 import android.database.Cursor;
-
 import com.tylersuehr.cleanarchitecture.data.models.Entity;
 /**
- * Copyright 2016 Tyler Suehr
- * Created by tyler on 12/25/2016.
+ * Copyright 2017 Tyler Suehr
+ * Created by tyler on 7/3/2017.
+ *
+ * <b>Summary</b>
+ * We encapsulate the mapping logic for our models for the SQLite database.
  */
 public interface IEntityMapper<T extends Entity> {
+    /**
+     * Maps {@link Cursor} data to an {@link Entity} object.
+     * @param c {@link Cursor}
+     * @return {@link Entity} object
+     */
     T map(Cursor c);
-    ContentValues toContentValues(T entity);
+
+    /**
+     * Maps an {@link Entity} object to {@link ContentValues} to store in SQLite database.
+     * @param entity {@link Entity} object
+     * @return {@link ContentValues}
+     */
+    ContentValues map(T entity);
 }
---
+++ app/src/main/java/com/tylersuehr/cleanarchitecture/data/mappers/PersonMapper.java
@@ -0,0 +1,32 @@
+package com.tylersuehr.cleanarchitecture.data.mappers;
+import android.content.ContentValues;
+import android.database.Cursor;
+import com.tylersuehr.cleanarchitecture.data.models.Person;
+import static com.tylersuehr.cleanarchitecture.data.repositories.DatabaseContract.People;
+/**
+ * Copyright 2017 Tyler Suehr
+ * Created by tyler on 7/3/2017.
+ */
+public class PersonMapper implements IEntityMapper<Person> {
+    @Override
+    public Person map(Cursor c) {
+        Person person = new Person();
+        person.setId(c.getString(c.getColumnIndex(People.COL_ID)));
+        person.setFirstName(c.getString(c.getColumnIndex(People.COL_FIRST_NAME)));
+        person.setLastName(c.getString(c.getColumnIndex(People.COL_LAST_NAME)));
+        person.setImage(c.getString(c.getColumnIndex(People.COL_IMAGE)));
+        person.setAge(c.getInt(c.getColumnIndex(People.COL_AGE)));
+        return person;
+    }
+
+    @Override
+    public ContentValues map(Person entity) {
+        ContentValues values = new ContentValues();
+        values.put(People.COL_ID, entity.getId());
+        values.put(People.COL_FIRST_NAME, entity.getFirstName());
+        values.put(People.COL_LAST_NAME, entity.getLastName());
+        values.put(People.COL_IMAGE, entity.getImage());
+        values.put(People.COL_AGE, entity.getAge());
+        return values;
+    }
+}
