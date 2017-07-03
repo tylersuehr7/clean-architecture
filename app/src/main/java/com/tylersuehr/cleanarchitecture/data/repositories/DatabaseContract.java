067954a2bb469f78f977a3635fd961e14d5c10d2
Tyler Suehr <tylersuehr7@yahoo.com>
2017-07-03 13:15:20 -0400

    Created a contract for storing people

--- app/src/main/java/com/tylersuehr/cleanarchitecture/data/repositories/DatabaseContract.java
+++ app/src/main/java/com/tylersuehr/cleanarchitecture/data/repositories/DatabaseContract.java
@@ -1,58 +1,16 @@
 package com.tylersuehr.cleanarchitecture.data.repositories;
 
 /**
- * Copyright 2016 Tyler Suehr
- * Created by tyler on 12/25/2016.
+ * Copyright 2017 Tyler Suehr
+ * Created by tyler on 7/3/2017.
  */
 public final class DatabaseContract {
-    public static final String NAME = "clean_architecture_db";
-    public static final int VERSION = 1;
-
-
-    public static abstract class Users {
-        public static final String NAME = "users";
-        public static final String COL_ID = "id";
-        public static final String COL_FIRSTNAME = "firstName";
-        public static final String COL_LASTNAME = "lastName";
-        public static final String COL_USERNAME = "username";
-        public static final String COL_PASSWORD = "password";
-        static final String CREATE = "CREATE TABLE [users] (" +
-                "[id] TEXT UNIQUE PRIMARY KEY," +
-                "[firstName] TEXT," +
-                "[lastName] TEXT," +
-                "[username] TEXT," +
-                "[password] TEXT);";
-    }
-
-    public static abstract class Phones {
-        public static final String NAME = "phones";
-        public static final String COL_ID = "id";
-        public static final String COL_MAKE = "make";
-        public static final String COL_MODEL = "model";
-        public static final String COL_CARRIER = "carrier";
-        public static final String COL_PRICE = "price";
-        static final String CREATE = "CREATE TABLE [phones] (" +
-                "[id] TEXT UNIQUE PRIMARY KEY," +
-                "[make] TEXT," +
-                "[model] TEXT," +
-                "[carrier] TEXT," +
-                "[price] REAL);";
-    }
-
-    public static abstract class Cars {
-        public static final String NAME = "cars";
-        public static final String COL_ID = "id";
-        public static final String COL_MAKE = "make";
-        public static final String COL_MODEL = "model";
-        public static final String COL_COLOR = "color";
-        public static final String COL_YEAR = "year";
-        public static final String COL_IS_USED = "isUsed";
-        static final String CREATE = "CREATE TABLE [cars] (" +
-                "[id] TEXT UNIQUE PRIMARY KEY," +
-                "[make] TEXT," +
-                "[model] TEXT," +
-                "[color] TEXT," +
-                "[year] INTEGER," +
-                "[isUsed] INTEGER DEFAULT 0);";
+    public static abstract class People {
+        public static final String NAME = "people";
+        public static final String COL_ID = "pId";
+        public static final String COL_FIRST_NAME = "pFirst";
+        public static final String COL_LAST_NAME = "pLast";
+        public static final String COL_IMAGE = "pImage";
+        public static final String COL_AGE = "pAge";
     }
 }
