7efb94a132d7ff7bae54c66914117300fef4cf30
Tyler Suehr <tylersuehr7@yahoo.com>
2017-07-04 13:38:39 -0400

    QueryUtils will not throw the EmptyQueryException

--- app/src/main/java/com/tylersuehr/cleanarchitecture/ui/shared/Mock.java
+++ app/src/main/java/com/tylersuehr/cleanarchitecture/ui/shared/Mock.java
@@ -8,7 +8,7 @@ import java.util.Random;
  */
 public final class Mock {
     public static String randomImage() {
-        String[] images = new String[] { "img_profile_0", "img_profile_1", "img_profile_2",
+        String[] images = new String[] { "img_profile_0", "img_profile_2",
                 "img_profile_3", "img_profile_4", "img_profile_5" };
         return images[new Random().nextInt(images.length)];
     }
