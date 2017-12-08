4e50ed734d60b2d97c1469c724e93fb9575d54ce
Tyler <tylersuehr@gmail.com>
2017-12-08 11:03:20 -0500

    Removed list and single callback

--- app/src/main/java/com/tylersuehr/cleanarchitecture/data/Injection.java
+++ app/src/main/java/com/tylersuehr/cleanarchitecture/data/Injection.java
@@ -1,23 +1,25 @@
 package com.tylersuehr.cleanarchitecture.data;
+import android.app.Activity;
 import android.content.Context;
 import com.tylersuehr.cleanarchitecture.data.mappers.PersonMapper;
 import com.tylersuehr.cleanarchitecture.data.repositories.DatabaseClient;
-import com.tylersuehr.cleanarchitecture.data.repositories.people.IPersonRepository;
 import com.tylersuehr.cleanarchitecture.data.repositories.people.LocalPersonRepository;
 import com.tylersuehr.cleanarchitecture.data.repositories.people.PersonRepository;
+
 /**
  * Copyright 2017 Tyler Suehr
- * Created by tyler on 7/3/2017.
  *
- * <b>Summary</b>
  * We use this to provide production instances of our repositories.
+ * Note: ensure to use {@link Activity#getApplicationContext()} only to prevent
+ *       memory leakage!!
  *
- * <b>Important</b>
- * Make sure to use getApplicationContext() only, so we don't leak memory!!!!
+ * @author Tyler Suehr
+ * @version 1.0
  */
 public final class Injection {
-    public static PersonRepository providePeopleRepo(Context c) {
-        IPersonRepository local = new LocalPersonRepository(DatabaseClient.getInstance(c), new PersonMapper());
-        return PersonRepository.getInstance(local);
+    public static PersonRepository providePeopleRepo(Context appCtx) {
+        return PersonRepository.getInstance(new LocalPersonRepository(
+                DatabaseClient.getInstance(appCtx), new PersonMapper()
+        ));
     }
 }
